8764b9aad8e363f876e38e54bb62741b443efd9a
xxx <644203591@qq.com>
2019-06-21 18:08:14 +0800

    add file about deconstruct

---
+++ 析构函数/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace 析构函数
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle(5, 10);
+            rect.Deconstruct(out float width, out float height);
+            Console.WriteLine($"Hello{width},{height} World!");
+            Console.ReadKey();
+        }
+    }
+
+    public class Rectangle    {        public readonly float Width, Height;        public Rectangle(float width, float height)        {            Width = width;            Height = height;        }
+    }
+
+    public static class Extension
+    {
+        public static void Deconstruct(this Rectangle rectangle,out float width,out float height)
+        {
+            width = rectangle.Width;
+            height = rectangle.Height;
+
+        }
+    }
+}
