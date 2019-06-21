74e0f31cf8ec876d300b00fad5f58eee8779e697
xxx <644203591@qq.com>
2019-06-21 17:15:49 +0800

    2nd edit

--- AsyncTask/Program.cs
+++ AsyncTask/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Net;
 
 namespace AsyncTask
 {
@@ -7,9 +6,24 @@ namespace AsyncTask
     {
         static void Main(string[] args)
         {
-            WebClient webClient = new WebClient();
-            var result=webClient.DownloadString("http://www.daochuzhao.com/sample-page");
-            Console.WriteLine(result);
+            //WebClient webClient = new WebClient();
+            //var result=webClient.DownloadString("http://www.daochuzhao.com/sample-page");
+            //Console.WriteLine(result);
+            Rectangle rectangle = new Rectangle(10.4f,12.4f);
+            rectangle.Deconstruct(out var width, out var height);
+            Console.WriteLine($"{width}, {height}") ;
+            Console.ReadKey();
         }
+    }    public class Rectangle    {        public readonly float Width, Height;        public Rectangle(float width, float height)        {            Width = width;            Height = height;        }
+
+        //在该类型内部声明析构函数
+        //public void Deconstruct(out float width,out float height)
+        //{
+        //    width = Width;
+        //    height = Height;
+        //}
     }
+
+    //k扩展方法声明类型的析构函数
+    public static class Extension    {        public static void Deconstruct(this Rectangle rect, out float width, out float height)        {            width = rect.Width;            height = rect.Height;        }    }
 }
